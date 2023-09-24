8a7b5b7c0fadfc4b2931d69635eb79ccf0f280be
wrote-code <developer@noreply.email.com>
2023-09-25 02:20:57 +0800

    dubbo使用zookeeper做注册中心

--- chapter4/src/main/java/com/wrotecode/spring/springcloud/cp4/userservice/UserProvider.java
+++ chapter4/src/main/java/com/wrotecode/spring/springcloud/cp4/userservice/UserProvider.java
@@ -3,7 +3,8 @@ package com.wrotecode.spring.springcloud.cp4.userservice;
 public class UserProvider implements UserApi {
     @Override
     public String getNameById(String id) {
-        System.out.println("请求id:" + id);
-        return "用户名:" + id;
+        String host = System.getProperty("host");
+        System.out.printf("请求实例:%s, 请求参数: %s%n", host, id);
+        return String.format("实例:%s, 用户标识:%s", host, id);
     }
 }
--- chapter4/src/main/java/com/wrotecode/spring/springcloud/cp4/userservice/UserServiceApp.java
+++ chapter4/src/main/java/com/wrotecode/spring/springcloud/cp4/userservice/UserServiceApp.java
@@ -1,5 +1,6 @@
 package com.wrotecode.spring.springcloud.cp4.userservice;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.util.Scanner;
@@ -10,6 +11,7 @@ public class UserServiceApp {
         context.load("classpath:user-provider.xml");
         context.refresh();
         System.out.println(context);
+        System.out.printf("实例%s已启动%n", System.getProperty("host"));
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
