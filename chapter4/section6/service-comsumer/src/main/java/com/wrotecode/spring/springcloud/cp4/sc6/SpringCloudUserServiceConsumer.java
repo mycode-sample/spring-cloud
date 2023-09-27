24f0627ebe7067e246ae7b2e26aeb585ec378aa2
wrote-code <developer@noreply.email.com>
2023-09-28 01:27:20 +0800

    增加消费端

---
+++ chapter4/section6/service-comsumer/src/main/java/com/wrotecode/spring/springcloud/cp4/sc6/SpringCloudUserServiceConsumer.java
@@ -0,0 +1,19 @@
+package com.wrotecode.spring.springcloud.cp4.sc6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class SpringCloudUserServiceConsumer {
+    public static void main(String[] args) throws Exception {
+        ConfigurableApplicationContext ctx = SpringApplication.run(SpringCloudUserServiceConsumer.class, args);
+        UserServiceApi api = ctx.getBean(UserServiceApi.class);
+        while (true) {
+            api.getNameById(new Date().toString());
+            Thread.sleep(1000);
+        }
+    }
+}
